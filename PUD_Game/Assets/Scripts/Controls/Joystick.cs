b99bb898c077b5e9ed2370755a32fdf746a8d524
nbutler34 <42753334+nbutler34@users.noreply.github.com>
2021-07-14 18:22:54 -0400

    Grappling

--- PUD_Game/Assets/Scripts/Controls/Joystick.cs
+++ PUD_Game/Assets/Scripts/Controls/Joystick.cs
@@ -5,89 +5,173 @@ using UnityEngine.EventSystems;
 
 public class Joystick : MonoBehaviour
 {
+    //General
     public Transform player; //pud
+    Vector2 lastLocation;
+
+    //Joystick & Touch Controls
     public float speed = 2.0f;
-    private bool touchStart = false;
+    private bool touchStart = false; //whether or not the joystick is being used
     private Vector2 pointA;
     private Vector2 pointB;
-
     public Transform circle; //touch
     public Transform outerCircle; //threshold
-
     public GameObject joystickZone; //panel
-
-    public List<touchLocation> touches = new List<touchLocation>();
-
+    public List<touchLocation> touches = new List<touchLocation>(); //finger ID index
     public int joystickID = 99;
 
+    //Grappling
     private bool grappled = false;
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float maxDistance = 10f;
+    public LayerMask mask;
+    public float step = 0.01f;
+    public LineRenderer line;
+    public bool actionUnreleased;
+    Rigidbody2D playerRB;
+    public float swingSpeed = 5f;
+    
 
     void Start()
     {
-        
+        joint = player.GetComponent<DistanceJoint2D>(); //sets joint to Pud's distance joint
+        joint.enabled = false;
+        grappled = false;
+        actionUnreleased = false;
+        line.enabled = false;
+        playerRB = player.GetComponent<Rigidbody2D>();
+        lastLocation = player.position;
     }
 
     void Update()
     {
+        if(player.position.x > lastLocation.x)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if(player.position.x < lastLocation.x)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (line.enabled)
+        {
+            UpdateLineEnd();
+        }
+        if (grappled)
+        {
+            //playerRB.AddForce(player.transform.up * -swingSpeed * 2);
+            playerRB.drag = 0.5f;
+        }
+        else
+        {
+            playerRB.drag = 0;
+        }
+        if (actionUnreleased)
+        {
+            ShrinkRope(false);
+        }
+
+
+
         int i = 0;
         while (i < Input.touchCount)
         {
             Touch t = Input.GetTouch(i);
+
             if (t.phase == TouchPhase.Began)
             {
-                touches.Add(new touchLocation(t.fingerId, joystickZone));
+                touches.Add(new touchLocation(t.fingerId, joystickZone)); //adds the touch to the ID index
 
-                if (joystickCheck(i))
+                if (joystickCheck(i)) //if inside the joystick zone,
                 {
-                    pointA = outerCircle.transform.position;
+                    pointA = outerCircle.transform.position; //sets a base position for the joystick
 
-                    circle.transform.position = pointA;
+                    circle.transform.position = pointA; //sets the circle to the middle of the joystick
 
-                    joystickID = i;
+                    joystickID = i; //set this finger ID to control the joystick
+                }
+                else if (!grappled)
+                {
+                    targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, 0)); //stores the position of the tap in world space
+                    ShootRope();
+                }
+                else if (grappled)
+                {
+                    BreakRope();
                 }
             }
             else if (t.phase == TouchPhase.Ended)
             {
-                if(i == joystickID)
+                if (i == joystickID) //if this finger is controlling the joystick, all variables are set to their defaults
                 {
                     touchStart = false;
 
                     circle.transform.position = outerCircle.transform.position;
                     joystickID = 99;
+
+                }
+                else if (grappled)
+                {
+                    actionUnreleased = false;
                 }
 
-                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId);
-                touches.RemoveAt(touches.IndexOf(thisTouch));
+                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId); //finds the finger in the ID index
+                touches.RemoveAt(touches.IndexOf(thisTouch)); //and removes it
             }
             else if (t.phase == TouchPhase.Moved)
             {
-                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId);
+                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId); //find the finger in the ID index
 
-                if (i == joystickID)
+                if (i == joystickID) //if this finger is controlling the joystick
                 {
                     touchStart = true;
-                    pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, Camera.main.transform.position.z));
+                    pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, Camera.main.transform.position.z)); //sets the postion of the joystick
+                }
+                else if (grappled)
+                {
+                    
                 }
             }
             ++i;
         }
+
+        lastLocation = player.position;
     }
 
     private void FixedUpdate()
     {
-        if (touchStart) //moves the circle part of the joystick and calls the move character function
+        if (touchStart) //if there is a finger on the joystick, it moves the player
         {
             Vector2 offset = pointB - pointA;
             Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
-            moveCharacter(direction);
+            //moveCharacter(direction);
+
+            if(grappled)
+            {
+                if(direction.x > 0.4)
+                {
+                    playerRB.AddForce(player.right * swingSpeed);
+                }
+                else if(direction.x < -0.4)
+                {
+                    playerRB.AddForce(player.right * -swingSpeed);
+                }
+
+                if (direction.y > 0.6)
+                {
+                    ShrinkRope(true);
+                }
+                else if (direction.y < -0.6)
+                {
+                    GrowRope(true);
+                }
+            }
 
             circle.transform.position = new Vector2(pointA.x + direction.x, pointA.y + direction.y);
         }
-    }
-
-    void moveCharacter(Vector2 direction) //...
-    {
-        player.Translate(direction * speed * Time.deltaTime);
     }
 
     bool joystickCheck(int iD) //use a raycast at the given touch id to detect if it's in the joystick zone
@@ -97,15 +181,76 @@ public class Joystick : MonoBehaviour
         RaycastHit2D hit;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(iD).position);
-        if(hit = Physics2D.Raycast(ray.origin, new Vector2(0, 0)))
+        if (hit = Physics2D.Raycast(ray.origin, new Vector2(0, 0)))
         {
-            if(hit.collider.name == joystickZone.name)
+            if (hit.collider.name == joystickZone.name)
             {
                 output = true;
             }
         }
 
         return output;
+    }
+
+    void ShootRope()
+    {
+        actionUnreleased = true;
+
+        hit = Physics2D.Raycast(player.position, targetPos - player.position, maxDistance, mask); //stores the hit object
+
+        if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null) //if the raycast hits an object with a rigidbody2d,
+        {
+            joint.enabled = true;
+
+            Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y); //gets the corrected anchor point
+
+            joint.connectedAnchor = connectPoint; //sets anchor point to the corrected value
+            joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>(); //connects the hit rigibody2d
+            joint.distance = Vector2.Distance(player.position, hit.point); //sets the distance between player and hit point
+
+            line.enabled = true;
+            line.SetPosition(0, player.position);
+            line.SetPosition(1, hit.point);
+
+            grappled = true;
+        }
+    }
+
+    void BreakRope()
+    {
+        joint.enabled = false;
+        line.enabled = false;
+        grappled = false;
+    }
+
+    void ShrinkRope(bool joystick)
+    {
+        if (joystick)
+        {
+            joint.distance -= 2*step;
+        }
+        else
+        {
+            joint.distance -= step;
+        }
+        
+    }
+
+    void GrowRope(bool joystick)
+    {
+        if (joystick)
+        {
+            joint.distance += 2 * step;
+        }
+        else
+        {
+            joint.distance += step;
+        }
+    }
+
+    void UpdateLineEnd()
+    {
+        line.SetPosition(0, player.position - line.transform.position);
     }
 
     Vector2 getTouchPosition(Vector2 touchPosition) //idk if we need this
---
+++ PUD_Game/Assets/Scripts/Controls/old/GrappleScript.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GrappleScript : MonoBehaviour
+{
+    public LineRenderer line;
+    DistanceJoint2D joint;
+    public GameObject aimObject;
+    public float maxDistance = 10f;
+    public float step = 0.2f;
+    public float fixedRotation = 0;
+    public float orbitSpeed = 350.0f;
+    public float swingSpeed = 200f;
+    public float buttonPressRate = 0.1f;
+    public LayerMask layerMask;
+    public bool actionUnreleased;
+    public bool isSwinging;
+    public bool gameOver;
+    Rigidbody2D playerRB;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    Transform player;
+    public GameObject gameOverScreen;
+    public GameObject winScreen;
+    float timeLastPressed;
+
+    void Start()
+    {
+        //Initialize variables
+        joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        line.enabled = false;
+        player = transform;
+        actionUnreleased = false;
+        isSwinging = false;
+        playerRB = GetComponent<Rigidbody2D>();
+        gameOver = false;
+        timeLastPressed = 0f;
+    }
+
+    void Update()
+    {
+        //MAINTENANCE
+
+        if (!gameOver) //checks if the game is over
+        {
+            //Locks player rotation
+            LockRotation();
+
+            if (line.enabled)
+            {
+                //Updates the location of the end of the rope to the player's location if the line is enabled
+                UpdateLineEnd();
+                //keeps the aim indicator in line with the rope
+                alignAim();
+            }
+
+            if (joint.distance > 1f && actionUnreleased)
+            {
+                //Shrinks the rope if the player hasn't released the action button and it isn't the shortest length
+                ShrinkRope();
+            }
+
+            if (isSwinging)
+            {
+                //Adds force to make swinging easier
+                playerRB.AddForce(transform.up * -swingSpeed * 2);
+                playerRB.drag = 0.5f;
+            }
+            else
+            {
+                //Sets no drag if not swinging to allow more distance to be covered
+                playerRB.drag = 0;
+            }
+        }
+
+
+
+
+        //MECHANICS
+
+        if (Input.GetKeyUp(KeyCode.Space)) //checks if the action button is released
+        {
+            if (gameOver) { SceneManager.LoadScene(1); } //restarts the scene if the game is over
+            else //if the game isn't over it sets the 2 variables to their correct vaules
+            {
+                actionUnreleased = false;
+                if (joint.enabled) { isSwinging = true; }
+            }
+        }
+
+        if (!gameOver) //checks if the game isn't over
+        {
+            if (Input.GetKeyDown(KeyCode.E)) //checks if the action button is pressed down
+            {
+                if (isSwinging) { BreakRope(); } //breaks the rope if there is one
+                else if (Time.time > (timeLastPressed + buttonPressRate)) { ShootRope(); } //shoots a new rope if there isn't one and it's been long enough
+
+                timeLastPressed = Time.time; //stores the time the button was pressed
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow)) //checks if both left and right are pressed
+            {
+                GrowRope();
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow)) //checks if the left button is pressed
+            {
+                if (isSwinging) { playerRB.AddForce(transform.right * -swingSpeed); } //adds force to the left if swinging
+                else { OrbitAround(orbitSpeed); } //spins the aim indicator if not
+            }
+            else if (Input.GetKey(KeyCode.RightArrow)) //checks if the right button is pressed
+            {
+                if (isSwinging) { playerRB.AddForce(transform.right * swingSpeed); } //adds force to the right if swinging
+                else { OrbitAround(-orbitSpeed); } //spins the aim indicator if not
+            }
+        }
+    }
+
+    void OrbitAround(float ObjectSpeed)
+    {
+        aimObject.transform.RotateAround(transform.position, Vector3.forward, ObjectSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("KillZone"))
+        {
+            //Debug.Log("regigster collosion");
+            gameOverScreen.SetActive(true);
+            gameOver = true;
+        }
+
+        if (collision.gameObject.tag.Equals("WinZone"))
+        {
+            //Debug.Log("regigster collosion");
+            winScreen.SetActive(true);
+            gameOver = true;
+
+        }
+    }
+
+    void LockRotation()
+    {
+        //player.eulerAngles = new Vector3(player.eulerAngles.x, player.eulerAngles.y, player.eulerAngles.z);
+        //example of unlocking rotation axes
+
+        player.eulerAngles = new Vector3(fixedRotation, fixedRotation, fixedRotation);
+        //locks all player rotation axes
+    }
+
+    void BreakRope()
+    {
+        joint.enabled = false;
+        line.enabled = false;
+        isSwinging = false;
+    }
+
+    void ShootRope()
+    {
+        actionUnreleased = true;
+
+        targetPos = aimObject.transform.position;
+        targetPos.z = 0;
+
+        hit = Physics2D.Raycast(transform.position, targetPos - transform.position, maxDistance, layerMask);
+
+        if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+        {
+            joint.enabled = true;
+
+            Vector2 connectPoint = hit.point - new Vector2(hit.collider.transform.position.x, hit.collider.transform.position.y);
+            connectPoint.x = connectPoint.x / hit.collider.transform.localScale.x;
+            connectPoint.y = connectPoint.y / hit.collider.transform.localScale.y;
+
+            joint.connectedAnchor = connectPoint;
+            joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+            joint.distance = Vector2.Distance(transform.position, hit.point);
+
+            line.enabled = true;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, hit.point);
+        }
+    }
+
+    void UpdateLineEnd()
+    {
+        line.SetPosition(0, transform.position - line.transform.position);
+    }
+
+    void ShrinkRope()
+    {
+        joint.distance -= step;
+    }
+
+    void GrowRope()
+    {
+        joint.distance += step;
+    }
+
+    void alignAim()
+    {
+        float correctedHitpointX = line.GetPosition(1).x - transform.position.x;
+        float correctedHitpointY = line.GetPosition(1).y - transform.position.y;
+        //Debug.Log(correctedHitpointX);
+        //Debug.Log(correctedHitpointY);
+
+        float calculatedPositionX = 0f;
+        float calculatedPositionY = 0f;
+
+        if (correctedHitpointX != 0 && correctedHitpointY != 0)
+        {
+            if (correctedHitpointX > 0)
+            {
+                if (correctedHitpointY > 0)
+                {
+                    calculatedPositionX = 1.273f * Mathf.Sqrt((correctedHitpointY * correctedHitpointY) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                    calculatedPositionY = 1.273f * Mathf.Sqrt((correctedHitpointX * correctedHitpointX) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                }
+                else if (correctedHitpointY < 0)
+                {
+                    calculatedPositionX = -1.273f * Mathf.Sqrt((correctedHitpointY * correctedHitpointY) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                    calculatedPositionY = 1.273f * Mathf.Sqrt((correctedHitpointX * correctedHitpointX) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                }
+            }
+            else if (correctedHitpointX < 0)
+            {
+                if (correctedHitpointY > 0)
+                {
+                    calculatedPositionX = 1.273f * Mathf.Sqrt((correctedHitpointY * correctedHitpointY) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                    calculatedPositionY = -1.273f * Mathf.Sqrt((correctedHitpointX * correctedHitpointX) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                }
+                else if (correctedHitpointY < 0)
+                {
+                    calculatedPositionX = -1.273f * Mathf.Sqrt((correctedHitpointY * correctedHitpointY) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                    calculatedPositionY = -1.273f * Mathf.Sqrt((correctedHitpointX * correctedHitpointX) / ((correctedHitpointX * correctedHitpointX) + (correctedHitpointY * correctedHitpointY)));
+                }
+            }
+
+            //aimObject.transform.position = new Vector3(calculatedPositionX + transform.position.x, calculatedPositionY + transform.position.y, 0f);
+            aimObject.transform.position = new Vector3(calculatedPositionY + transform.position.x, calculatedPositionX + transform.position.y, 0f); //I had to flip the x and y for some reason??????
+        }
+    }
+}
